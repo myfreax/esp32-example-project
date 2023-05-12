e6234fb4db3d34252c69675a14c9dbd521277a3c
myfreax <web@myfreax.com>
2023-05-12 16:26:35 +0800

    feat: intergate buzzer and led in button's lift event callback function

--- components/button/button.c
+++ components/button/button.c
@@ -1,27 +1,37 @@
 #include "button.h"
 
-#include "adc.h"
 #include "buzzer.h"
 #include "esp_log.h"
 #include "led.h"
 
 static const char* TAG = "BUTTON";
 
-void button_usb_press(int64_t time_us) {
-  ESP_LOGI(TAG, "usb button press time: %lld", time_us);
+void button_usb_press(int64_t time_us, bool state) {
+  ESP_LOGI(TAG, "usb button press time: %lld state: %d", time_us, state);
 }
-void button_usb_lift(int64_t time_us) {
-  ESP_LOGI(TAG, "usb button lift time: %lld", time_us);
+
+void button_usb_lift(int64_t time_us, bool state) {
+  ESP_LOGI(TAG, "usb button lift time: %lld state: %d", time_us, state);
+  gpio_set_level(CONFIG_LED_USB_PIN, state);
+  buzzer_once(100 * 1000);
 }
-void button_ac_press(int64_t time_us) {
-  ESP_LOGI(TAG, "ac button press time: %lld", time_us);
+
+void button_ac_press(int64_t time_us, bool state) {
+  ESP_LOGI(TAG, "ac button press time: %lld state: %d", time_us, state);
 }
-void button_ac_lift(int64_t time_us) {
-  ESP_LOGI(TAG, "ac button lift time: %lld", time_us);
+
+void button_ac_lift(int64_t time_us, bool state) {
+  ESP_LOGI(TAG, "ac button lift time: %lld state: %d", time_us, state);
+  gpio_set_level(CONFIG_LED_AC_PIN, state);
+  buzzer_once(100 * 1000);
 }
-void button_dc_press(int64_t time_us) {
-  ESP_LOGI(TAG, "dc button press time: %lld", time_us);
+
+void button_dc_press(int64_t time_us, bool state) {
+  ESP_LOGI(TAG, "dc button press time: %lld state: %d", time_us, state);
 }
-void button_dc_lift(int64_t time_us) {
-  ESP_LOGI(TAG, "dc button lift time: %lld", time_us);
+
+void button_dc_lift(int64_t time_us, bool state) {
+  ESP_LOGI(TAG, "dc button lift time: %lld state: %d", time_us, state);
+  gpio_set_level(CONFIG_LED_DC_PIN, state);
+  buzzer_once(100 * 1000);
 }
